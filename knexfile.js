ddc3b7da4cfbad1b6897f2e766d387a208c7cfe8
sammzamm <sammzamm01@gmail.com>
2017-03-28 09:17:24 -0600

    migrations and seeding

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,17 @@
 'use strict';
 
-module.exports = {
-  development: {},
 
-  test: {},
+
+module.exports = {
+  development: {
+    client: 'pg',
+connection: 'postgres://localhost/bookshelf_dev'
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/bookshelf_test'
+  },
 
   production: {}
 };
